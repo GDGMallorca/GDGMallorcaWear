7d98aa923784a65f0f002f9898456888b92cc6d7
Juan Miguel Liras <sekongur@frikigard.com>
2014-07-28 21:48:55 +0200

    voice recognition on opening attendee list

--- wear/src/main/java/com/gdgmallorcawear/AttendeesActivity.java
+++ wear/src/main/java/com/gdgmallorcawear/AttendeesActivity.java
@@ -1,14 +1,18 @@
 package com.gdgmallorcawear;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.view.DelayedConfirmationView;
 import android.support.wearable.view.WearableListView;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class AttendeesActivity extends Activity implements WearableListView.ClickListener,
@@ -21,6 +25,8 @@ public class AttendeesActivity extends Activity implements WearableListView.Clic
     private static Activity sContext;
     private ArrayList<Event> mEvents = new ArrayList<Event>();
     private static final int NUM_SECONDS = 2;
+    private static final int SPEECH_REQUEST_CODE = 0;
+
 
 
     @Override
@@ -50,6 +56,9 @@ public class AttendeesActivity extends Activity implements WearableListView.Clic
 
         sContext = this;
 
+        //Start the speech recognition
+        displaySpeechRecognizer();
+
     }
 
     @Override
@@ -68,6 +77,31 @@ public class AttendeesActivity extends Activity implements WearableListView.Clic
             }
         });
 
+    }
+
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            // Do something with spokenText
+            Log.d("JM","Texto:"+spokenText);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 
